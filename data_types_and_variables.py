af4044495e67857e6280e8944094af8a9a7c9611
brandon <brandon.t.navarrete@gmail.com>
2022-11-03 14:09:54 -0500

    class anwsers add

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -1,6 +1,7 @@
-m = 3
-b = 5
-h = 1
+m = 3 #mermaid
+b = 5#brother bear
+h = 1#hercules
+
 
 print((m * 3) + (b * 3) + (h * 3 ))
 
@@ -10,20 +11,22 @@ g = 400 #google
 a = 380 #amazon
 f = 350 #facebook
 
+
 print((f * 10) + (g * 6) + (a * 4))
 #----------------------
-class_full = False
-class_interfers = False
+class_not_full = True
+class_doest_conflict= True
+student_can_enroll = class_not_full and class_doest_conflict
 
-student_can_enroll = class_full and not class_interfers
 
 print (student_can_enroll)
 
 #----------------------
 people_buy_two = True
 offer_not_expired = True
+premium_member = False
 
-can_be_applied = people_buy_two and offer_not_expired
+can_be_applied = offer_not_expired and (people_buy_two or premium_member)
 
 print (can_be_applied)
 
@@ -48,6 +51,9 @@ safe_pass = pass_char_stro and right_size and no_match
 
 safe_pass
 
+#check_1 = len(password)>=5
+#check_2 = len(username) <20
+#check_3 = username != password
 
 
 #if <condition>:
