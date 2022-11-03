89560879f5e7138b16e2f6aa0a1ef23bbd400c8e
brandon <brandon.t.navarrete@gmail.com>
2022-11-03 13:42:14 -0500

    corrected answer

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -27,5 +27,31 @@ can_be_applied = people_buy_two and offer_not_expired
 
 print (can_be_applied)
 
+#BOOLEAN----
+username = 'codeup'
+password = 'notastrongpassword'
 
+safe_pass = None
+
+#measure length of password
+if (len(password)) >= 5:
+    pass_char_stro = True
+#measure length of username
+if (len(username)) <= 20:
+    right_size = True
+# testing equality of password and username
+if password != username:
+    no_match = True
+
+#setting safe_pass value
+safe_pass = pass_char_stro and right_size and no_match
+
+safe_pass
+
+
+
+#if <condition>:
+ #   var = True
+#else:
+ #   var = False
 
