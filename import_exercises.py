cc428ea8adf8040b588eed4ee5e03fe77333a20d
brandon <brandon.t.navarrete@gmail.com>
2022-11-08 13:51:55 -0600

    update

---
+++ import_exercises.py
@@ -0,0 +1,26 @@
+#using sqrt, ceil, pow. we need to import math
+#import math
+
+#x = 4
+#math.sqrt(x)
+#print(x)
+
+#import math as m
+#x = 16
+#m.sqrt(x)
+#print(x)
+
+#x = 4.3
+#z = m.ceil(4.3) # return the ceiling of x, i.e. round UP!
+#print(z)
+
+#import pandas as pd
+#pd.Series(['a',1,True])
+#-------
+from function_exercises import is_vowel, calculate_tip as c_t
+
+is_vowel("a")
+
+#-------
+
+c_t(5,25)
